ac5b834fd5ad1c682ea4cccc3e6049602a8e59e7
Priyanka Ajaykumar Thakker <pthakker@isimtl.local>
2019-10-16 20:14:11 -0400

    final commit

--- index.php
+++ index.php
@@ -15,20 +15,72 @@ $forms = [
 <head>
 <meta charset="utf-8" />
 <title>Form building</title>
+<style>
+div {
+    background-color:#45a049;
+    width:30%;
+
+}
+
+header,label{
+    color:white;
+}
+input[type=text],input[type=password], select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+</style>
+
 </head>
 <body>
 <div>
 <header>
-<h1></h1>
+<h1>add input in array</h1>
 </header>
 <main>
-<form action="<?php echo $forms[0]["action"] ?>" method="<?php echo $forms[0]["action"] ?>">
 <?php 
-foreach($forms as $property=>$val){
-    <input 
+$i=0;
+$string="<form ";
+$cntfields=count($forms);
+
+foreach ($forms as $inputs) {
+    $propstr="";
+    $cnt=count($inputs);
     
+    foreach ($inputs as $property=>$val) {
+        if($cnt==1){
+            $label=$val;
+        }else{
+            $propstr.=$property."=".$val." ";
+        }
+        $cnt--;
+    }
+    if (strpos($propstr,"submit") !== false) {
+        $string.="<br>";
+    }
+     $cntfields--;
+    if ($i==0) {
+        $string.=$propstr.">\n";
+    }
+    else {
+        if($cntfields!=0||$cntfields==count($forms)){
+            $string.="<label>".$label."</label>";
+    
+        }
+        $string.= "<input ".$propstr.">\n";
+    }
+   
+    $i++;
 }
+echo $string;
 ?>
+
 </form>
 </main>
 </div>
